052cdec069f8b4f19bffc43ebe84a0d2e34a22e1
Sasekani <sasngoveni@gmail.com>
2026-09-09 22:29:35 +0200

    Initial project setup

---
+++ src/test/java/com/sasekani/budget_management_system/BudgetManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sasekani.budget_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
